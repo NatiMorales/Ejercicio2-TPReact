b882ffb9b4129de5c5ecdaf6092167e01a4a0b02
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-12 18:03:07 -0300

    se limpio la estructura del proyecto y se instalaron las dependencies de react-bootstrap y bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { Container } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <></> 
+  );
+}
+
+export default App
