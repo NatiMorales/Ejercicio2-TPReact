1ced21e145ea8f1e30edc0e44003628c68b746f6
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-12 18:45:27 -0300

    se agrego la etiqueta section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-
 import TituloPrincipal from './components/TituloPrincipal';
 
 function App() {
 
   return (
-    <>
+    <section>
       <TituloPrincipal texto = "my friend"></TituloPrincipal>
-    </>
+    </section>
   );
 }
 
